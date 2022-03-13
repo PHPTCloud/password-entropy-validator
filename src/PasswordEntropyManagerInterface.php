4a52b65468fef67d9697fee4bc8d592f28836553
Алексей Юдов <tcloud.ax@gmail.com>
2022-03-13 07:05:58 +0300

    inti

---
+++ src/PasswordEntropyManagerInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PasswordEntropyBundle;
+
+/**
+ * @author tcloud.ax <tcloud.ax@gmail.com>
+ * @since  v1.0.0
+ */
+interface PasswordEntropyManagerInterface
+{
+}
